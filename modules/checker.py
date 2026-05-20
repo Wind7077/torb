b3862aeb45a14e0d62e4a31cdae2dee1377d2e32
Miros <92308835+Wind7077@users.noreply.github.com>
2026-05-20 18:55:19 +0300

    Update checker.py

--- modules/checker.py
+++ modules/checker.py
@@ -1,18 +1,28 @@
 import asyncio
+import ssl
 
 SEM = asyncio.Semaphore(300)
 
 
-async def tcp_check(host: str, port: int, timeout=15):
+async def tcp_check(host: str, port: int, timeout=15, use_tls=False):
 
     try:
 
         async with SEM:
 
-            _, writer = await asyncio.wait_for(
-                asyncio.open_connection(host, port),
-                timeout=timeout
-            )
+            if use_tls:
+                ctx = ssl.create_default_context()
+                ctx.check_hostname = False
+                ctx.verify_mode = ssl.CERT_NONE
+                _, writer = await asyncio.wait_for(
+                    asyncio.open_connection(host, port, ssl=ctx),
+                    timeout=timeout
+                )
+            else:
+                _, writer = await asyncio.wait_for(
+                    asyncio.open_connection(host, port),
+                    timeout=timeout
+                )
 
             writer.close()
 
