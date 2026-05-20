5af6b0b5055b55fb47b8620b32632698e56c411e
Miros <92308835+Wind7077@users.noreply.github.com>
2026-05-20 17:46:42 +0300

    Update latency.py

--- modules/latency.py
+++ modules/latency.py
@@ -1,1 +1,22 @@
+import asyncio
+import time
+
+async def measure_latency(host: str, port: int, timeout=5):
+    try:
+        start = time.perf_counter()
+
+        reader, writer = await asyncio.wait_for(
+            asyncio.open_connection(host, port),
+            timeout=timeout
+        )
+
+        latency = round((time.perf_counter() - start) * 1000)
+
+        writer.close()
+        await writer.wait_closed()
+
+        return latency
+
+    except:
+        return 99999
 
