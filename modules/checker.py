61ce6f8065cbe175e2aad73efc7fe4df1a4fdb50
Miros <92308835+Wind7077@users.noreply.github.com>
2026-05-20 18:54:26 +0300

    Update checker.py

--- modules/checker.py
+++ modules/checker.py
@@ -3,20 +3,18 @@ import asyncio
 SEM = asyncio.Semaphore(300)
 
 
-async def tcp_check(host: str, port: int, timeout=8):
+async def tcp_check(host: str, port: int, timeout=15):
 
     try:
 
         async with SEM:
 
-            reader, writer = await asyncio.wait_for(
+            _, writer = await asyncio.wait_for(
                 asyncio.open_connection(host, port),
                 timeout=timeout
             )
 
             writer.close()
-
-            await writer.wait_closed()
 
             return True
 
